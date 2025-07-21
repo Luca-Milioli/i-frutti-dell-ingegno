682ef1ac082f8bd5acff17f504a448d287e6b92a
FedericoRigolon <318199@studenti.unimore.it>
2025-07-21 10:44:57 +0200

    set_last

--- frutti_dell_ingegno/scripts/gui/components/equation/equation_container.gd
+++ frutti_dell_ingegno/scripts/gui/components/equation/equation_container.gd
@@ -18,3 +18,10 @@ func setup(equation: Equation):
 	
 	var eq = preload("res://scenes/components/equation/sign.tscn").instantiate()
 	eq.set_texture(load(PATH_TO_SIGN + "=.png"))
+	add_child(eq)
+	
+	var res = preload("res://scenes/components/equation/rhs.tscn").instantiate()
+	res.set_text(equation.calculate_result())
+
+func setup_last_one() -> void:
+	$Rhs.set_text("??")
