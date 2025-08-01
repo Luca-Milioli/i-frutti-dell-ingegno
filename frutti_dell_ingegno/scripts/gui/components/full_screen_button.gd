98a57aca6d4e2ebba2fe9dd0dd4843f09a704f47
FedericoRigolon <318199@studenti.unimore.it>
2025-08-01 11:58:56 +0200

    fullscreen

--- frutti_dell_ingegno/scripts/gui/components/full_screen_button.gd
+++ frutti_dell_ingegno/scripts/gui/components/full_screen_button.gd
@@ -1,24 +1,23 @@
 extends TextureButton
 
 
-func _ready() -> void:
-	if not is_fullscreen_available():
-		self.queue_free()
-
-
-func is_fullscreen_available() -> bool:
-	if OS.has_feature("debug"):
-		return false
-	return true
-
-
-func _toggle_fullscreen() -> bool:  # return ture if it sets fullscreen
+func _toggle_fullscreen() -> void:  # return ture if it sets fullscreen
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		return false
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	return true
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 
 func _on_pressed() -> void:
 	_toggle_fullscreen()
+	await get_tree().process_frame
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		var hover = preload("res://art/graphics/buttons/FullscreenHover.png")
+		set("texture_normal", preload("res://art/graphics/buttons/Fullscreen.png"))
+		set("texture_hover", hover)
+		set("texture_pressed", hover)
+	else:
+		var hover = preload("res://art/graphics/buttons/FullscreenHover.png")
+		set("texture_normal", preload("res://art/graphics/buttons/Fullscreen.png"))
+		set("texture_hover", hover)
+		set("texture_pressed", hover)
