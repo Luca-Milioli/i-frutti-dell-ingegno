aeeba04355f53e27738656aed41dabb1373ea876
FedericoRigolon <318199@studenti.unimore.it>
2025-09-01 16:53:34 +0200

    no FullScreen mobile

--- frutti_dell_ingegno/scripts/gui/components/full_screen_button.gd
+++ frutti_dell_ingegno/scripts/gui/components/full_screen_button.gd
@@ -1,6 +1,17 @@
-## Manages FullScreen or WindowMode
+## Manages FullScreen or Windowed Mode.
 extends TextureButton
 class_name FullScreenButton
+
+
+## If user is using a mobile device, this button will be removed.
+func _ready() -> void:
+	var device = ""
+	for arg in OS.get_cmdline_args():
+		if arg.begins_with("--device="):
+			device = arg.split("=")[1]
+	
+	if device == "mobile":
+		queue_free()
 
 
 ## Set fullscreen if it's windowed and possible. If it's fullscreen it sets windowed.
@@ -25,3 +36,8 @@ func _on_pressed() -> void:
 		set("texture_normal", preload("res://art/graphics/buttons/Fullscreen.png"))
 		set("texture_hover", hover)
 		set("texture_pressed", hover)
+
+
+## When button is released, it forces Normal Texture (to fix a bug from tablet and mobile). 
+func _on_button_up() -> void:
+	set_pressed_no_signal(false)
--- frutti_dell_ingegno/scripts/main.gd
+++ frutti_dell_ingegno/scripts/main.gd
@@ -6,7 +6,7 @@ class_name Main
 const URL = "https://spreafico.net/"
 
 
-## Makes the background transparent
+## Makes the background transparent.
 func _ready() -> void:
 	get_tree().root.transparent_bg = true
 
